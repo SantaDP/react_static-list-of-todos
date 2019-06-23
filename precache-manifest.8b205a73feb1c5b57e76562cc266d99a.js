5459ad0b49997f36f5abe4b29d0f432fcc6fc622
Aleksandr Pereleev <aleksandr.pereleev@gmail.com>
2019-06-23 17:40:35 +0300

    Updates

---
+++ precache-manifest.8b205a73feb1c5b57e76562cc266d99a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b77e3f3bdabda7ed7901c4ae26e9f8a",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "299af0e41e6f3c66575e",
+    "url": "/react_static-list-of-todos/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react_static-list-of-todos/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "299af0e41e6f3c66575e",
+    "url": "/react_static-list-of-todos/static/js/main.fe91f32c.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_static-list-of-todos/static/media/logo.5d5d9eef.svg"
+  }
+]);
